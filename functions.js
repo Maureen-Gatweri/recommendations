3a2467b096af523c20a8d26c3e73b1f908a22630
Maureen-Gatweri <maureennmwendwa@gmail.com>
2024-05-03 07:06:09 +0300

    make changes

--- functions.js
+++ functions.js
@@ -24,13 +24,13 @@ const RecommendationSystem = {
   };
   
   // Add users and their preferred items
-  RecommendationSystem.addUser("user1", ["item1", "item2", "item3"]);
-  RecommendationSystem.addUser("user2", ["item4", "item5", "item6"]);
+  RecommendationSystem.addUser("client1", ["item1", "item2", "item3"]);
+  RecommendationSystem.addUser("client2", ["item4", "item5", "item6"]);
   
   // Get recommendations for a user
-  console.log(RecommendationSystem.getRecommendations("user1"));
-  console.log(RecommendationSystem.getRecommendations("user2"));
-  console.log(RecommendationSystem.getRecommendations("user3"));
+  console.log(RecommendationSystem.getRecommendations("client1"));
+  console.log(RecommendationSystem.getRecommendations("client2"));
+  console.log(RecommendationSystem.getRecommendations("client3"));
   
   
   
